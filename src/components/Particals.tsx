d3fc7dcbf06e167bc35da47899442dd9143aa491
VindexTOS <giorgikutateladze1998@gmail.com>
2023-12-23 00:19:36 +0400

    fix robot lagging

--- src/components/Particals.tsx
+++ src/components/Particals.tsx
@@ -1,22 +1,22 @@
-import { useEffect, useState } from "react";
-import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { useEffect, useState } from 'react'
+import Particles, { initParticlesEngine } from '@tsparticles/react'
 // import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
 // import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+import { loadSlim } from '@tsparticles/slim' // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
 // import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
 
 const Particals = () => {
-  const [init, setInit] = useState(false);
+  const [init, setInit] = useState(false)
 
   useEffect(() => {
     // Initialize tsParticles engine
     initParticlesEngine(async (engine) => {
       // Load the slim version of tsParticles
-      await loadSlim(engine);
+      await loadSlim(engine)
     }).then(() => {
-      setInit(true);
-    });
-  }, []);
+      setInit(true)
+    })
+  }, [])
 
   return (
     <Particles
@@ -24,16 +24,16 @@ const Particals = () => {
       options={{
         zIndex: -10,
         background: {},
-        fpsLimit: 120,
+        fpsLimit: 60,
         interactivity: {
           events: {
             onClick: {
               enable: true,
-              mode: "push",
+              mode: 'push',
             },
             onHover: {
               enable: true,
-              mode: "repulse",
+              mode: 'repulse',
             },
           },
           modes: {
@@ -48,20 +48,20 @@ const Particals = () => {
         },
         particles: {
           color: {
-            value: "#FF0000",
+            value: '#FF0000',
           },
           links: {
-            color: "#FF0000",
+            color: '#FF0000',
             distance: 180,
             enable: true,
             opacity: 0.5,
             width: 1,
           },
           move: {
-            direction: "none",
+            direction: 'none',
             enable: true,
             outModes: {
-              default: "bounce",
+              default: 'bounce',
             },
             random: false,
             speed: 1,
@@ -77,7 +77,7 @@ const Particals = () => {
             value: 0.1,
           },
           shape: {
-            type: "circle",
+            type: 'circle',
           },
           size: {
             value: { min: 1, max: 5 },
@@ -86,7 +86,7 @@ const Particals = () => {
         detectRetina: true,
       }}
     />
-  );
-};
+  )
+}
 
-export default Particals;
+export default Particals
--- src/context/RobotContext.tsx
+++ src/context/RobotContext.tsx
@@ -4,137 +4,133 @@ import {
   useEffect,
   useReducer,
   useState,
-} from "react";
-import { UseNavigationContext } from "./NavigationContext";
+} from 'react'
+import { UseNavigationContext } from './NavigationContext'
 
 type RobotAction = {
-  type: string;
-  payload: string | number | boolean;
-};
+  type: string
+  payload: string | number | boolean
+}
 type RobotState = {
-  closeNav: boolean;
-  closeTerminal: boolean;
-  RobotSwtich: string;
-  robotStart: boolean;
-};
+  closeNav: boolean
+  closeTerminal: boolean
+  RobotSwtich: string
+  robotStart: boolean
+}
 
 type Cell = {
-  robotState: RobotState;
-  RobotDispatch: React.Dispatch<RobotAction>;
-  resultText: string[];
-  isRobotActive: boolean;
-  setIsRobotActive: React.Dispatch<React.SetStateAction<boolean>>;
-};
+  robotState: RobotState
+  RobotDispatch: React.Dispatch<RobotAction>
+  resultText: string[]
+  isRobotActive: boolean
+  setIsRobotActive: React.Dispatch<React.SetStateAction<boolean>>
+}
 
-const RobotContext = createContext<Cell | null>(null);
+const RobotContext = createContext<Cell | null>(null)
 
 export const RobotContextProvider = ({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) => {
-  const { locationRef } = UseNavigationContext();
-  const [isRobotActive, setIsRobotActive] = useState(true);
+  const { locationRef } = UseNavigationContext()
+  const [isRobotActive, setIsRobotActive] = useState(true)
   const initialState = {
     closeNav: false,
     closeTerminal: false,
     robotStart: false,
-    RobotSwtich: "PROMTS",
-  };
+    RobotSwtich: 'PROMTS',
+  }
   const robotReducer = (state: RobotState, action: RobotAction) => {
     switch (action.type) {
-      case "CLOSE_TERMINAL":
+      case 'CLOSE_TERMINAL':
         return {
           ...state,
           closeTerminal: action.payload as boolean,
-        };
-      case "CLOSE_NAV":
-        return { ...state, closeNav: !state.closeNav };
-      case "ROBOT_SWTICH":
+        }
+      case 'CLOSE_NAV':
+        return { ...state, closeNav: !state.closeNav }
+      case 'ROBOT_SWTICH':
         return {
           ...state,
           RobotSwtich: action.payload as string,
-        };
-      case "CLOSE_TERMINAL_AND_NAV":
+        }
+      case 'CLOSE_TERMINAL_AND_NAV':
         return {
           ...state,
           closeNav: false,
           closeTerminal: true,
-        };
-      case "CONTACT":
-        return { ...state, RobotSwtich: action.payload as string };
+        }
+      case 'CONTACT':
+        return { ...state, RobotSwtich: action.payload as string }
 
-      case "START_ROBOT":
-        return { ...state, robotState: !state.robotStart };
+      case 'START_ROBOT':
+        return { ...state, robotState: !state.robotStart }
       default:
-        return state;
+        return state
     }
-  };
-  const [robotState, RobotDispatch] = useReducer(robotReducer, initialState);
+  }
+  const [robotState, RobotDispatch] = useReducer(robotReducer, initialState)
 
   // ROBOT PROMPTS
 
-  const [resultText, setResultText] = useState<string[]>(["$ "]);
+  const [resultText, setResultText] = useState<string[]>(['$ '])
   const RobotPromps = {
-    hello:
-      " გამარჯობა, კეთილი იყოს თქვენი მობრძანება Redline Technologies".split(
-        " "
-      ),
-    iWilLHelP: "დააკლიკე სტარტ ღილაკს და დაგეხმარები :)".split(" "),
-    services:
-      "ძაალიან კარგი შემოთავაზებები და სერვისები აქვთ, ბოლო-ბოლო მე გამაკეთეს :)".split(
-        " "
-      ),
-    team: "ნახეთ რა ვაჟკაცები გვყავს გუნდში, დათვებივით ბიჭებიი".split(" "),
-    project:
-      "თუ გინდა რომ შენი პროექტიც მოხვდეს აქ დაგვიკავშირდით: info@redline.ink ".split(
-        " "
-      ),
-    contact:
-      "დაგვიკავშირდით info@redline.ink  ან მოგვწერეთ facebook-ზე ლინკი ქვემოთ არის O_o".split(
-        " "
-      ),
-  };
+    hello: ' გამარჯობა, კეთილი იყოს თქვენი მობრძანება Redline Technologies'.split(
+      ' ',
+    ),
+    iWilLHelP: 'დააკლიკე სტარტ ღილაკს და დაგეხმარები :)'.split(' '),
+    services: 'ძაალიან კარგი შემოთავაზებები და სერვისები აქვთ, ბოლო-ბოლო მე გამაკეთეს :)'.split(
+      ' ',
+    ),
+    team: 'ნახეთ რა ვაჟკაცები გვყავს გუნდში, დათვებივით ბიჭებიი'.split(' '),
+    project: 'თუ გინდა რომ შენი პროექტიც მოხვდეს აქ დაგვიკავშირდით: info@redline.ink '.split(
+      ' ',
+    ),
+    contact: 'დაგვიკავშირდით info@redline.ink  ან მოგვწერეთ facebook-ზე ლინკი ქვემოთ არის O_o'.split(
+      ' ',
+    ),
+  }
   const changeText = (mappingText: string[]) => {
-    let index = 0;
+    let index = 0
 
     const intervalId = setInterval(() => {
       if (index < mappingText.length) {
-        const nextWord = mappingText[index];
+        const nextWord = mappingText[index]
         if (nextWord !== undefined) {
           setResultText((prevResultText: string[]) => [
             ...prevResultText,
             `${nextWord} `,
-          ]);
+          ])
         }
-        index++;
+        index++
       } else {
-        clearInterval(intervalId);
+        clearInterval(intervalId)
       }
-    }, 200);
-  };
+    }, 200)
+  }
 
   useEffect(() => {
-    console.log(locationRef);
-    setResultText([]);
+    console.log(locationRef)
+    setResultText([])
     switch (locationRef) {
-      case "service":
-        changeText(RobotPromps.services);
-        break;
-      case "team":
-        changeText(RobotPromps.team);
-        break;
-      case "project":
-        changeText(RobotPromps.project);
-        break;
-      case "contact":
-        changeText(RobotPromps.contact);
-        break;
+      case 'service':
+        changeText(RobotPromps.services)
+        break
+      case 'team':
+        changeText(RobotPromps.team)
+        break
+      case 'project':
+        changeText(RobotPromps.project)
+        break
+      case 'contact':
+        changeText(RobotPromps.contact)
+        break
       default:
-        changeText(RobotPromps.hello);
-        break;
+        changeText(RobotPromps.hello)
+        break
     }
-  }, [locationRef]);
+  }, [locationRef])
 
   return (
     <RobotContext.Provider
@@ -148,14 +144,14 @@ export const RobotContextProvider = ({
     >
       {children}
     </RobotContext.Provider>
-  );
-};
+  )
+}
 
 export const UseRobotContext = () => {
-  const context = useContext(RobotContext);
+  const context = useContext(RobotContext)
   if (!context) {
-    throw new Error("Context Not Wrapped Reload Page");
+    throw new Error('Context Not Wrapped Reload Page')
   }
 
-  return context;
-};
+  return context
+}
