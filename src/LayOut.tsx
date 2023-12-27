2f3b6268604ed25124ab8e725388f6e61352e60f
vindexTOS <giorgikutateladze1998@gmail.com>
2023-12-27 17:56:07 +0400

    removing robot on phone mode

--- src/LayOut.tsx
+++ src/LayOut.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import NavBar from './components/NavBar/NavBar'
-import Particals from './components/Particals'
-import Home from './components/Home/Home'
-import Robot from './components/Robot/Robot'
-import Services from './components/Services/Services'
-import Team from './components/Team/Team'
-import Projects from './components/Projects/Projects'
-import Contact from './components/Contact/Contact'
+import React from "react";
+import NavBar from "./components/NavBar/NavBar";
+import Particals from "./components/Particals";
+import Home from "./components/Home/Home";
+import Robot from "./components/Robot/Robot";
+import Services from "./components/Services/Services";
+import Team from "./components/Team/Team";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
 
 function LayOut() {
-  const style = { main: `w-[100vw] h-[100%] relative bg-[#171312]` }
+  const style = { main: `w-[100vw] h-[100%] relative bg-[#171312]` };
   return (
     <main className={style.main}>
       <NavBar />
@@ -22,7 +22,7 @@ function LayOut() {
       <Projects />
       <Contact />
     </main>
-  )
+  );
 }
 
-export default LayOut
+export default LayOut;
--- src/components/Robot/Robot.tsx
+++ src/components/Robot/Robot.tsx
@@ -1,63 +1,61 @@
-import React, { useEffect, useReducer, useRef, useState } from 'react'
-import Lottie from 'react-lottie'
-import { motion as m, useAnimation } from 'framer-motion'
-import animationData from '../../assets/animations/Robot.json'
-import Terminal from './Terminal'
-import Prompts from './Prompts'
-import RobotNav from './RobotNav'
-import InterFace from '../../assets/icons/interface.png'
-import Loca from '../../assets/icons/location.png'
-import { UseRobotContext } from '../../context/RobotContext'
-import Contacts from './Contacts'
-import { useWindowScroll } from 'react-use'
-import StartIcon from '../../assets/icons/play.png'
-import StopIcon from '../../assets/icons/stop-button.png'
-import Glootie1 from '../../assets/audio/wanna_develop_an_app_1.mp3'
-import { BsRobot } from 'react-icons/bs'
+import React, { useEffect, useReducer, useRef, useState } from "react";
+import Lottie from "react-lottie";
+import { motion as m, useAnimation } from "framer-motion";
+import animationData from "../../assets/animations/Robot.json";
+import Terminal from "./Terminal";
+import Prompts from "./Prompts";
+import RobotNav from "./RobotNav";
+import InterFace from "../../assets/icons/interface.png";
+import Loca from "../../assets/icons/location.png";
+import { UseRobotContext } from "../../context/RobotContext";
+import Contacts from "./Contacts";
+import { useWindowScroll } from "react-use";
+import StartIcon from "../../assets/icons/play.png";
+import StopIcon from "../../assets/icons/stop-button.png";
+import Glootie1 from "../../assets/audio/wanna_develop_an_app_1.mp3";
+import { BsRobot } from "react-icons/bs";
 
 function Robot() {
-  const {
-    robotState,
-    RobotDispatch,
-    isRobotActive,
-    setIsRobotActive,
-  } = UseRobotContext()
+  const { robotState, RobotDispatch, isRobotActive, setIsRobotActive } =
+    UseRobotContext();
 
   // drag animations
 
   const screenSize = {
     width: window.innerWidth,
     height: window.innerHeight,
-  }
+  };
 
   const dragConstraints = {
     left: 0,
     right: screenSize.width - 500,
     top: 0,
     bottom: screenSize.height - 333,
-  }
+  };
   const style = {
-    mainDiv: `flex absolute  z-50 ${!isRobotActive && 'hidden'}`,
-    section: ` w-[100%] h-[100%] `,
+    mainDiv: `flex absolute  max_smm1:hidden  z-50 ${
+      !isRobotActive && "hidden"
+    }`,
+    section: ` w-[100%] h-[100%] max_smm1:hidden `,
     iconWrapper: `absolute flex gap-2 left-[14rem] top-[-1.2rem] `,
     icon: `w-[40px] h-[40px] cursor-pointer z-50`,
-  }
+  };
 
-  const [scrollY, setScrollY] = React.useState(10)
-  const { y: pageYOffset } = useWindowScroll()
+  const [scrollY, setScrollY] = React.useState(10);
+  const { y: pageYOffset } = useWindowScroll();
 
   React.useEffect(() => {
-    setScrollY(pageYOffset)
-  }, [pageYOffset])
+    setScrollY(pageYOffset);
+  }, [pageYOffset]);
 
   const defaultOptions = {
     loop: true,
     autoplay: true,
     animationData: animationData,
     rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
+      preserveAspectRatio: "xMidYMid slice",
     },
-  }
+  };
   //   robot terminal views
   // const RobotTerminalViews = () => {
   // 	switch (robotState.RobotSwtich) {
@@ -72,53 +70,53 @@ function Robot() {
   // 	}
   // };
   //   robbot start stoper state
-  const [stopStart, setStopStart] = useState<boolean>(true)
+  const [stopStart, setStopStart] = useState<boolean>(true);
   // robbot audio
 
-  const audioRef = useRef(null)
+  const audioRef = useRef(null);
 
   // robbot buttons
   const handleNav = () => {
-    RobotDispatch({ type: 'ROBOT_SWTICH', payload: 'NAV' })
-    RobotDispatch({ type: 'CLOSE_NAV', payload: '' })
-    RobotDispatch({ type: 'CLOSE_TERMINAL', payload: false })
-  }
+    RobotDispatch({ type: "ROBOT_SWTICH", payload: "NAV" });
+    RobotDispatch({ type: "CLOSE_NAV", payload: "" });
+    RobotDispatch({ type: "CLOSE_TERMINAL", payload: false });
+  };
   // robot moivng randomliy
 
   const [robotPosition, setRobotPosition] = useState({
     x: screenSize.width - 2000,
     y: screenSize.height - 800,
-  })
+  });
 
   const directions = [
     { x: 1, y: 1 },
     { x: -1, y: 1 },
     { x: 1, y: -1 },
     { x: -1, y: -1 },
-  ]
+  ];
 
-  let currentDirectionIndex = Math.floor(Math.random() * directions.length)
+  let currentDirectionIndex = Math.floor(Math.random() * directions.length);
 
   const moveRobot = () => {
-    const direction = directions[currentDirectionIndex]
-    let newX = robotPosition.x + direction.x * (screenSize.width - 100)
-    let newY = robotPosition.y + direction.y * (screenSize.height - 133)
+    const direction = directions[currentDirectionIndex];
+    let newX = robotPosition.x + direction.x * (screenSize.width - 100);
+    let newY = robotPosition.y + direction.y * (screenSize.height - 133);
 
     // Check and adjust direction when hitting any boundary
     if (newX <= 0 || newX >= screenSize.width - 100) {
-      direction.x *= -1 // Reverse the x direction
-      currentDirectionIndex = (currentDirectionIndex + 1) % directions.length
+      direction.x *= -1; // Reverse the x direction
+      currentDirectionIndex = (currentDirectionIndex + 1) % directions.length;
     }
     if (newY <= 0 || newY >= screenSize.height - 133) {
-      direction.y *= -1 // Reverse the y direction
-      currentDirectionIndex = (currentDirectionIndex + 1) % directions.length
+      direction.y *= -1; // Reverse the y direction
+      currentDirectionIndex = (currentDirectionIndex + 1) % directions.length;
     }
 
-    setRobotPosition({ x: newX, y: newY })
-    console.log(`X: ${newX}, Y: ${newY}, Direction: ${currentDirectionIndex}`)
-  }
+    setRobotPosition({ x: newX, y: newY });
+    console.log(`X: ${newX}, Y: ${newY}, Direction: ${currentDirectionIndex}`);
+  };
 
-  const controls = useAnimation()
+  const controls = useAnimation();
 
   //   useEffect(() => {
   //     let timer: any
@@ -135,30 +133,30 @@ function Robot() {
   //   }, [stopStart])
 
   const toggleAudio = () => {
-    const audio: any = audioRef.current
+    const audio: any = audioRef.current;
     if (audio) {
       if (stopStart) {
-        audio.pause()
-        controls.stop()
+        audio.pause();
+        controls.stop();
       } else {
-        audio.play()
+        audio.play();
         // moveRobot()
       }
     }
-    setStopStart((prevIsPlaying) => !prevIsPlaying)
-  }
+    setStopStart((prevIsPlaying) => !prevIsPlaying);
+  };
 
   useEffect(() => {
-    const audio: any = audioRef.current
+    const audio: any = audioRef.current;
     if (audio && isRobotActive) {
       if (stopStart) {
-        audio.pause()
-        controls.stop()
+        audio.pause();
+        controls.stop();
       } else {
-        audio.play()
+        audio.play();
       }
     }
-  }, [isRobotActive])
+  }, [isRobotActive]);
   if (isRobotActive) {
     return (
       <m.div
@@ -169,7 +167,7 @@ function Robot() {
         initial={{ y: 140 }}
         dragElastic={true}
         dragMomentum={true}
-        transition={{ duration: 0.6, type: 'spring' }}
+        transition={{ duration: 0.6, type: "spring" }}
         className={style.mainDiv}
       >
         <Terminal>
@@ -206,7 +204,7 @@ function Robot() {
           isClickToPauseDisabled={true}
         />
       </m.div>
-    )
+    );
   } else {
     return (
       <BsRobot
@@ -214,8 +212,8 @@ function Robot() {
         onClick={() => setIsRobotActive(!isRobotActive)}
         className="text-[3rem] text-yellow-400 fixed left-3 cursor-pointer hover:text-yellow-500 top-8  z-50 "
       />
-    )
+    );
   }
 }
 
-export default Robot
+export default Robot;
